5c767ee3a6fa121749f086f5bcc7fc87ad92cb5f
Reinout Claeys <reinoutclaeys@gmail.com>
2018-02-17 13:09:04 +0100

    Make pyossCol configurable

--- src/app/pyoss-grid/directives/column.directive.ts
+++ src/app/pyoss-grid/directives/column.directive.ts
@@ -1,13 +1,27 @@
-import {Directive, HostBinding} from '@angular/core';
+import {Directive, HostBinding, Input, OnInit} from '@angular/core';
 
 @Directive({
     selector: '[pyossCol]',
 })
-export class ColumnDirective {
+export class ColumnDirective implements OnInit {
+
+    @Input('col')
+    private amount: number;
 
     @HostBinding('class')
-    private elementClass: string = 'col';
+    private elementClass: string;
 
     constructor() {
+    }
+
+    ngOnInit(): void {
+        this.elementClass = ColumnDirective.format('col', this.amount);
+    }
+
+    private static format(base: string, amount: number): string {
+        if(amount) {
+            return `${base}-${amount}`;
+        }
+        return base;
     }
 }
