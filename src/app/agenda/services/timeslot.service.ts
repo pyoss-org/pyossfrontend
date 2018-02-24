bf0744613f6414776a0a31f4f2f7c7a058247ab7
Drareeg <geerard.ponnet@gmail.com>
2018-02-24 22:05:08 +0100

    book a timeslot link

--- src/app/agenda/services/timeslot.service.ts
+++ src/app/agenda/services/timeslot.service.ts
@@ -12,6 +12,7 @@ export class TimeslotService {
 
     private getURL: string = environment.rootUrl + 'timeslots';
     private getFirstURL: string = environment.rootUrl + 'day/nextavailable';
+    private dobookingURL: string = environment.rootUrl + 'slot/book';
 
     constructor(private http: HttpClient) {
     }
@@ -24,4 +25,13 @@ export class TimeslotService {
       return this.http.get<Day> (this.getFirstURL);
   }
 
+  doBooking(slot: Timeslot) {
+      const data = {
+        fromH: slot.from.hour,
+        fromM: slot.from.minutes,
+        toH: slot.to.hour,
+        toM: slot.to.minutes
+      };
+    return this.http.post(this.dobookingURL, data);
+  }
 }
