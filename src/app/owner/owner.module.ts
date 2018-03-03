ba0de465c6bf683c289bc3f3f4d6ce73841f69cb
Reinout Claeys <reinoutclaeys@gmail.com>
2018-03-03 16:06:20 +0100

    remove unneeded imports

--- src/app/owner/owner.module.ts
+++ src/app/owner/owner.module.ts
@@ -1,15 +1,9 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {TimeslotService} from './services/timeslot.service';
-import {DayComponent} from './day/day.component';
-import {TimeslotComponent} from './timeslot/timeslot.component';
-import {AgendaComponent} from './agenda.component';
 import {PyossGridModule} from '../pyoss-grid/pyoss-grid.module';
 import {PyossHelperModule} from '../pyoss-helper/pyoss-helper.module';
 import {PyossFormModule} from '../pyoss-form/pyoss-form.module';
 import {PyossDisplayModule} from '../pyoss-display/pyoss-display.module';
-import {DayService} from './services/day.service';
-import {Logger} from './services/logger.service';
 import {OwnerComponent} from './owner/owner.component';
 import {OwnerService} from './owner/services/owner.service';
 
@@ -23,13 +17,13 @@ import {OwnerService} from './owner/services/owner.service';
         PyossHelperModule,
     ],
     declarations: [
-      OwnerComponent
+        OwnerComponent,
     ],
     providers: [
         OwnerService,
     ],
     exports: [
-      OwnerComponent
+        OwnerComponent,
     ],
 })
 
